95e01d26ee66b1f7710d75265e4d4b8eae09c120
mahdi <mahdi.rzg@gmail.com>
2021-11-26 02:59:37 +0100

    -

--- app/Http/Controllers/CronController.php
+++ app/Http/Controllers/CronController.php
@@ -9,6 +9,9 @@ use DOMDocument;
 use App\Models\AssetAccessories;
 use App\Models\Asset;
 use DOMXPath;
+use CloudflareBypass\CFCurlImpl;
+use CloudflareBypass\Model\UAMOptions;
+
 
 class CronController extends Controller
 {
@@ -132,15 +135,47 @@ class CronController extends Controller
         $historys = AssetHistory::where('sync', '=', 0)->get();
          foreach ($historys as $h) {
             $url = $h->trackurl;
-            $context = stream_context_create(
-                array(
-                    "http" => array(
-                        "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
-                    )
-                )
-            );
-            echo '<pre>';
-             $data = $this->cloudFlareBypass($url);
+
+                      $ch = curl_init($url);
+
+// Want to cache clearance cookies ?
+//curl_setopt($ch, CURLOPT_COOKIEJAR, "cookies.txt");
+//curl_setopt($ch, CURLOPT_COOKIEFILE, "cookies.txt");
+
+             curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+             curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+             curl_setopt($ch, CURLOPT_HTTPHEADER,
+                 array(
+                     "Upgrade-Insecure-Requests: 1",
+                     "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36",
+                     "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+                     "Accept-Language: en-US,en;q=0.9"
+                 ));
+
+             $cfCurl = new CFCurlImpl();
+
+             $cfOptions = new UAMOptions();
+             $cfOptions->setVerbose(true);
+// $cfOptions->setDelay(5);
+
+             try {
+                 $page = $cfCurl->exec($ch, $cfOptions);
+
+                 // Want to get clearance cookies ?
+                 //$cookies = curl_getinfo($ch, CURLINFO_COOKIELIST);
+
+             } catch (ErrorException $ex) {
+                 echo "Unknown error -> " . $ex->getMessage();
+             }
+
+
+
+
+
+
+
+
 
             var_dump($data);
             die();
@@ -161,81 +196,6 @@ class CronController extends Controller
     }
 
 
-    function cloudFlareBypass($url){
-
-        $useragent = "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/W.X.Y.Z‡ Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)";
-
-        $ct = curl_init();
-
-        curl_setopt_array($ct, Array(
-            CURLOPT_URL => $url,
-            CURLOPT_SSL_VERIFYPEER => false,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_HTTPHEADER => array("X-Requested-With: XMLHttpRequest"),
-            CURLOPT_REFERER => $url,
-            CURLOPT_USERAGENT =>  $useragent,
-            CURLOPT_HEADER => false,
-            CURLOPT_POST => true,
-            CURLOPT_POSTFIELDS => 'schn=csrf'
-        ));
-
-        $html = curl_exec($ct);
-
-        $dochtml = new DOMDocument();
-        @$dochtml->loadHTML($html);
-        $xpath = new DOMXpath($dochtml);
-
-        // Auth
-        if(isset($xpath->query("//input[@name='r']/@value")->item(0)->textContent)){
-            $action = $url . $xpath->query("//form/@action")->item(0)->textContent;
-            $r = $xpath->query("//input[@name='r']/@value")->item(0)->textContent;
-            $jschl_vc = $xpath->query("//input[@name='jschl_vc']/@value")->item(0)->textContent;
-            $pass = $xpath->query("//input[@name='pass']/@value")->item(0)->textContent;
-
-            // Generate curl post data
-            $post_data = array(
-                'r' => $r,
-                'jschl_vc' => $jschl_vc,
-                'pass' => $pass,
-                'jschl_answer' => ''
-            );
-
-            curl_close($ct); // Close curl
-
-            return $html;
-
-            $ct = curl_init();
-
-            // Post cloudflare auth parameters
-            curl_setopt_array($ct, Array(
-                CURLOPT_HTTPHEADER => array(
-                    'Accept: application/json, text/javascript, */*; q=0.01',
-                    'Accept-Language: ro-RO,ro;q=0.8,en-US;q=0.6,en-GB;q=0.4,en;q=0.2',
-                    'Referer: '. $url,
-                    'Origin: '. $url,
-                    'Content-Type: application/x-www-form-urlencoded; charset=UTF-8',
-                    'X-Requested-With: XMLHttpRequest'
-                ),
-                CURLOPT_URL => $action,
-                CURLOPT_REFERER => $url,
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_USERAGENT => $useragent,
-                CURLOPT_POSTFIELDS => http_build_query($post_data)
-
-            ));
-
-            $html_reponse = curl_exec($ct);
-
-            curl_close($ct); // Close curl
-
-        }else{
-
-            // Already auth
-            return $html;
-
-        }
-
-    }
 
 
 
