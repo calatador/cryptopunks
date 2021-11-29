3979cf11f615ee15b7a8587a9b43416de59b8dfe
mahdi <mahdi.rzg@gmail.com>
2021-11-29 02:25:19 +0100

    -

--- app/Console/Commands/CronHistory.php
+++ app/Console/Commands/CronHistory.php
@@ -3,6 +3,9 @@
 namespace App\Console\Commands;
 
 use App\Http\Controllers\CronController;
+use App\Models\Asset;
+use App\Models\AssetAccessories;
+use App\Models\Minlog;
 use Illuminate\Console\Command;
 
 class CronHistory extends Command
@@ -44,8 +47,89 @@ class CronHistory extends Command
         while (true){
         $c->syncHistory();
 
+            echo '-';
 
-        sleep(300);
+
+            $nbrDays = 10;
+            $name = 'Beanie';
+            $names = array("Beanie", "Choker", "Pilot Helmet", "Tiara", "Orange Side", "Buck Teeth", "Welding Goggles", "Pigtails", "Pink With Hat", "Top Hat", "Spots", "Rosy Cheeks", "Blonde Short", "Wild White Hair", "Cowboy Hat", "Wild Blonde", "Straight Hair Blonde", "Big Beard", "Red Mohawk", "Half Shaved", "Blonde Bob", "Vampire Hair", "Clown Hair Green", "Straight Hair Dark", "Straight Hair", "Silver Chain", "Dark Hair", "Purple Hair", "Gold Chain", "Medical Mask", "Tassle Hat", "Fedora", "Police Cap", "Clown Nose", "Smile", "Cap Forward", "Hoodie", "Front Beard Dark", "Frown", "Purple Eye Shadow", "Handlebars", "Blue Eye Shadow", "Green Eye Shadow", "Vape", "Front Beard", "Chinstrap", "3D Glasses", "Luxurious Beard", "Mustache", "Normal Beard Black", "Normal Beard", "Eye Mask", "Goat", "Do-rag", "Shaved Head", "Muttonchops", "Peak Spike", "Pipe", "VR", "Cap", "Small Shades", "Clown Eyes Green", "Clown Eyes Blue", "Headband", "Crazy Hair", "Knitted Cap", "Mohawk Dark", "Mohawk", "Mohawk Thin", "Frumpy Hair", "Wild Hair", "Messy Hair", "Eye Patch", "Stringy Hair", "Bandana", "Classic Shades", "Shadow Beard", "Regular Shades", "Horned Rim Glasses", "Big Shades", "Nerd Glasses", "Black Lipstick", "Mole", "Purple Lipstick", "Hot Lipstick", "Cigarette", "Earring");
+            $time_start = microtime(true);
+            $result = '';
+            set_time_limit(0);
+
+            $names = AssetAccessories::get();
+            foreach ( $names as $name) {
+                $name = $name->name;
+                $dateinit = date('Y-m-d', time());
+                $dateinit = date('Y-m-d', strtotime($dateinit . ' +' . 1 . ' day'));
+
+                $date = $dateinit;
+
+                for ($i = 0; $i < $nbrDays; $i++) {
+                    $nbr = ($i == 0) ? 0 : 1;
+                    $date = date('Y-m-d H:i:s', strtotime($date . ' -' . $nbr . ' day'));
+                    $datekey = date('Y-m-d', strtotime($date . ' -' . $nbr . ' day'));
+                    $min = null;
+                    $minlogtest = Minlog::where('accessorie' , '=', $name)
+                        ->where('date' , '=' , $datekey )->first();
+                    $assets = Asset::whereHas('accessoires', function ($q) use ($name) {
+                        $q->where('asset_accessories.name', '=', $name);
+                    })->get();
+                    foreach ($assets as $asset) {
+                        $as = $asset->dateCondition($date)->get();
+                        $asset->price = -1;
+                        foreach ($as as $s) {
+                            if (($s->type == 'Bid') or ($s->type == 'Bid *')) {
+
+                            } elseif (($s->type == 'Bid Withdrawn')) {
+
+                            } elseif ($s->type == 'Offered') {
+                                $asset->price = $s->eth;
+                                break;
+                            } elseif ($s->type == 'Sold') {
+                                break;
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+                    foreach ($assets as $key => $asset) {
+                        if ($asset->price == -1) {
+                            $assets->forget($key);
+                        }
+                    }
+                    $min = null;
+                    $ass = null;
+                    foreach ($assets as $asset) {
+                        if ($min == null) {
+                            $min = $asset->price;
+                            $ass = $asset;
+                        } else {
+                            if ($min > $asset->price) {
+                                $min = $asset->price;
+                                $ass = $asset;
+                            }
+                        }
+                    }
+                    if ($min != null) {
+                        $log = Minlog::where('date' , '=' , $datekey)->where('accessorie' , '=' ,$name)->first();
+                        if( $log instanceof  Minlog){
+                            $log->value = $min;
+                            $log->save();
+                        }else{
+                            $log = new Minlog();
+                            $log->date = $datekey;
+                            $log->accessorie = $name;
+                            $log->value = $min;
+                            $log->save();
+                        }
+                    }
+
+                }
+            }
+
+
+            sleep(300);
         }
         return Command::SUCCESS;
     }
