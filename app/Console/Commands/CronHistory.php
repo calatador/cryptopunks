a62713376e9ef48cc9a8269b67664eefb286f9de
mahdi <mahdi.rzg@gmail.com>
2021-11-29 02:58:49 +0100

    -

--- app/Console/Commands/CronHistory.php
+++ app/Console/Commands/CronHistory.php
@@ -44,20 +44,21 @@ class CronHistory extends Command
 
 
         $c = new CronController();
+        $hello = 0 ;
         while (true){
         $c->syncHistory();
 
-            echo '-';
-            $nbrDays = 3;
-            $name = 'Beanie';
-           // $names = array("Beanie", "Choker", "Pilot Helmet", "Tiara", "Orange Side", "Buck Teeth", "Welding Goggles", "Pigtails", "Pink With Hat", "Top Hat", "Spots", "Rosy Cheeks", "Blonde Short", "Wild White Hair", "Cowboy Hat", "Wild Blonde", "Straight Hair Blonde", "Big Beard", "Red Mohawk", "Half Shaved", "Blonde Bob", "Vampire Hair", "Clown Hair Green", "Straight Hair Dark", "Straight Hair", "Silver Chain", "Dark Hair", "Purple Hair", "Gold Chain", "Medical Mask", "Tassle Hat", "Fedora", "Police Cap", "Clown Nose", "Smile", "Cap Forward", "Hoodie", "Front Beard Dark", "Frown", "Purple Eye Shadow", "Handlebars", "Blue Eye Shadow", "Green Eye Shadow", "Vape", "Front Beard", "Chinstrap", "3D Glasses", "Luxurious Beard", "Mustache", "Normal Beard Black", "Normal Beard", "Eye Mask", "Goat", "Do-rag", "Shaved Head", "Muttonchops", "Peak Spike", "Pipe", "VR", "Cap", "Small Shades", "Clown Eyes Green", "Clown Eyes Blue", "Headband", "Crazy Hair", "Knitted Cap", "Mohawk Dark", "Mohawk", "Mohawk Thin", "Frumpy Hair", "Wild Hair", "Messy Hair", "Eye Patch", "Stringy Hair", "Bandana", "Classic Shades", "Shadow Beard", "Regular Shades", "Horned Rim Glasses", "Big Shades", "Nerd Glasses", "Black Lipstick", "Mole", "Purple Lipstick", "Hot Lipstick", "Cigarette", "Earring");
-            $names = array("Beanie");
-                $time_start = microtime(true);
+        if( $hello == 0){
+            $nbrDays = 365;
+        }else{
+            $nbrDays = 5;
+
+        }
+        $time_start = microtime(true);
             $result = '';
             set_time_limit(0);
 
             $names = AssetAccessories::get();
-            $names = AssetAccessories::where('name' , '=' , 'Beanie')->get();
 
             foreach ( $names as $name) {
                 $name = $name->name;
@@ -79,15 +80,6 @@ class CronHistory extends Command
                     foreach ($assets as $asset) {
                         $as = $asset->dateCondition($date)->get();
                         $asset->price = -1;
-                        echo "\n";
-
-
-                        if( $asset->num == 8865){
-                            foreach ($as as $s) {
-                                echo $s->type . '->' . $s->eth . "\n";
-                            }
-                        }
-
 
                         foreach ($as as $s) {
                             if (($s->type == 'Bid') or ($s->type == 'Bid *')) {
@@ -112,7 +104,6 @@ class CronHistory extends Command
                     $min = null;
                     $ass = null;
                     foreach ($assets as $asset) {
-                        echo $asset->id . '=>' .$asset->price . "\n";
                         if ($min == null) {
                             $min = $asset->price;
                             $ass = $asset;
@@ -124,6 +115,10 @@ class CronHistory extends Command
                         }
                     }
                     if ($min != null) {
+
+                    }else{
+                        $min = -1;
+                    }
                         echo $datekey . ' -> ' .$min;
                         $log = Minlog::where('date' , '=' , $datekey)->where('accessorie' , '=' ,$name)->first();
                         if( $log instanceof  Minlog){
@@ -136,7 +131,7 @@ class CronHistory extends Command
                             $log->value = $min;
                             $log->save();
                         }
-                    }
+
 
                 }
             }
