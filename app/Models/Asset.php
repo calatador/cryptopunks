a048bce00a2ec02935b767809971428fb24f08a5
mahdi <mahdi.rzg@gmail.com>
2021-12-04 19:26:17 +0100

    *

--- app/Models/Asset.php
+++ app/Models/Asset.php
@@ -216,10 +216,11 @@ class Asset extends Model
 
 
                  $peth = str_replace( ',' , '' , $prices[0] );
+                 $peth = number_format($peth * $f1 , 2 , '' ,'');
+
                  echo $peth;
 
                  die();
-                 $peth = number_format($peth * $f1 , 2 , '' ,'');
 
                  $pusd = str_replace( ',' , '' , $prices[1] );
                  $pusd = number_format($pusd * $f2 , 2 , '' ,'');
