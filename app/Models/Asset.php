94890077dac26866903e661358361e1107e391e4
mahdi <mahdi.rzg@gmail.com>
2021-12-04 19:24:59 +0100

    *

--- app/Models/Asset.php
+++ app/Models/Asset.php
@@ -215,6 +215,8 @@ class Asset extends Model
 
 
                  echo $prices[0];
+
+                 die();
                  $peth = str_replace( ',' , '' , $prices[0] );
                  $peth = number_format($peth * $f1 , 2 , '' ,'');
 
