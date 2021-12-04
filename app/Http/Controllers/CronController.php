2f58481cc373d58a9abda50263ffc511ca3dfec3
mahdi <mahdi.rzg@gmail.com>
2021-12-04 19:35:20 +0100

    -

--- app/Http/Controllers/CronController.php
+++ app/Http/Controllers/CronController.php
@@ -218,16 +218,10 @@ class CronController extends Controller
         ini_set('max_execution_time', 0);
         foreach ( $json_data as $key => $json) {
             $id = intval($key);
-            if( $id == 4722){
-                echo  $id .' --------';
-                Asset::assetSecInit($json, $id);
-
-            }
-           // $optionSatus->values = $id;
-           // $optionSatus->save();
+            Asset::assetSecInit($json, $id);
+            $optionSatus->values = $id;
+            $optionSatus->save();
         }
-        die();
-        echo 'done';
 
         $option = Options::where('option' , '=' , 'syncHistorySatus' )->first();
         if ($option instanceof Options){
