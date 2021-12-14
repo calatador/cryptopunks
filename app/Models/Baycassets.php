fb1a37f9479f46afbd5e58672ac381ad84e609ab
mahdi <mahdi.rzg@gmail.com>
2021-12-14 23:04:42 +0100

    -

---
+++ app/Models/Baycassets.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\AssetAccessories;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use DOMDocument;
+
+
+class Baycassets extends Model
+{
+    protected $table = 'asset_history';
+    use HasFactory;
+
+
+}
